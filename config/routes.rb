8b6ab8bf56ff40dfcb4ad39fb32cbe31d3247814
guillermo varela <guillovarela16@gmail.com>
2020-06-12 15:46:26 -0300

    Add new file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
-  get 'equipo', to: 'pages#equipo'
+  get 'team', to: 'pages#team'
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
