a46e53731a21ef786e0cbbc337c1ced2e87cc948
guillermo varela <guillovarela16@gmail.com>
2020-06-12 11:53:23 -0300

    Controller in progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,44 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index , :show]
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_articles
+     @article = Article.find(params[:id])
+  end
 end
