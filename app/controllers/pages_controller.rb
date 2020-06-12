8e6cbd7d0f969895dc14b8a50edce48906321899
guillermo varela <guillovarela16@gmail.com>
2020-06-12 14:58:01 -0300

    Finish index articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :about]
 
   def home
+    @articles = Article.all
   end
-
 
   def about
   end
