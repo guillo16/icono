bd3058b51d15426a8b2994437fb2d9c8214a10d4
guillermo varela <guillovarela16@gmail.com>
2020-06-12 15:08:10 -0300

    Finish articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_articles, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
   end
 
   def show
