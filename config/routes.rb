f32a0774152b9a7d04cc245cbb6a7029c294a568
guillermo varela <guillovarela16@gmail.com>
2020-06-16 09:57:15 -0300

    Create turnos page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'surgery', to: 'pages#surgery'
   get 'nutrition', to: 'pages#nutrition'
   get 'doctor', to: 'pages#doctor'
+  get 'turnos', to: 'pages#turnos'
   resources :articles
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
