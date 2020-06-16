feb5896f34c6223ac06643ab6e33dd485f32778a
guillermo varela <guillovarela16@gmail.com>
2020-06-16 10:43:18 -0300

    Create videos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/new'
+  get 'videos/create'
+  get 'videos/edit'
+  get 'videos/update'
+  get 'videos/destroy'
   get 'contacts/new'
   devise_for :users
   root to: 'pages#home'
