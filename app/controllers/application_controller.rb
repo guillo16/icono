730d0beb92852e1af2eda09500f3b92312fe8abd
guillermo varela <guillovarela16@gmail.com>
2020-09-30 20:41:12 -0300

    Finish follow 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || users_path
+  end
 end
